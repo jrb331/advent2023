3e6fdf626f4a68141f2a7e60debb86d280148750
jrb331 <jonathanrussellboyd@gmail.com>
2024-01-30 07:40:55 -0500

    day 12 part 1 solution

--- day12/day12-1.py
+++ day12/day12-1.py
@@ -1,6 +1,6 @@
 from itertools import product
 
-with open("sample.txt", "r") as f:
+with open("input.txt", "r") as f:
     lines = f.read().strip().split('\n')
 
 springList = []
@@ -10,10 +10,8 @@ for line in lines:
     springList.append(data[0])
     damagedSprings.append(list(map(int, data[1].split(","))))
 
-
-print(springList)
-print(damagedSprings)
-
+# print(springList)
+# print(damagedSprings)
 
 def combos(springs, target):
     line = []
@@ -26,18 +24,40 @@ def combos(springs, target):
             idxs.append(idx)
         elif char == '#':
             line.append(1)
-    
+    count = 0
     for combination in product([0, 1], repeat=len(idxs)):
         lineCopy = line.copy()
         for i, value in zip(idxs, combination):
            lineCopy[i] = value
            
-        print(lineCopy)
+        # print(lineCopy)
+        if(valid(lineCopy, target)):
+            count +=1
+    return count
 
-count = 0
+def valid(lineCopy, target):
+    contiguousSprings = []
+    count = 0
+    prev = ''
+    for num in lineCopy:
+        if num == 0:
+            if prev == 1:
+                contiguousSprings.append(count)
+                count = 0
+        elif num == 1:
+            count += 1
+        prev = num
+    if count > 0:
+        contiguousSprings.append(count)
+    # print(lineCopy, contiguousSprings)
+    # print(contiguousSprings == target)
+    return contiguousSprings == target
+
+total = 0
 for springs, target in list(zip(springList, damagedSprings)):
-    res = combos(springs, target)
-    count += res
+    result = combos(springs, target)
+    total += result
+print(total)
 
 # for line in allLocations:
     
